b6f9e2606dc994c723e98e0795a535e3178baf24
Ben Ku <benku12803@gmail.com>
2022-12-19 01:02:27 -1000

    added eventlister, finally input-form div

--- static/script.js
+++ static/script.js
@@ -1,22 +1,62 @@
 //store API url
-const API_URL = 'https://random-word-api.herokuapp.com/word?number=15'
-const wordDisplayElement = document.getElementById('quoteDisplay')
+const API_URL = 'https://random-word-api.herokuapp.com/word?number=40'
 
-function getWords() {
-    return fetch(API_URL)
-        .then(res => res.json())
-}
+// have html parsed first
+document.addEventListener("DOMContentLoaded", function(){
+    const wordDisplayElement = document.getElementById('wordBlock')
+    const wordInputElement = document.getElementById('wordInput')
 
-async function renderWords() {
-    const words = [];
-    words.push(await getWords());
-    text = "";
-    for (let i = 0; i < words.length; i++)
-    {
-        text += words[i];
-        text += " ";
+    wordInputElement.addEventListener('input', () => {
+        const arrayLetter = wordDisplayElement.querySelectorAll('span');
+        const arrayValue = wordInputElement.value.split('')
+        
+        
+        arrayLetter.forEach((characterSpan, index) => {
+            const character = arrayValue[index]
+            if (character == null) 
+            {
+                characterSpan.classList.remove('correct')
+                characterSpan.classList.remove('incorrect')
+            }
+            else if (character == characterSpan.innerText) {
+                characterSpan.classList.add('correct')
+                characterSpan.classList.remove('incorrect')
+            }
+            else 
+            {
+                characterSpan.classList.add('incorrect')
+                characterSpan.classList.remove('correct')
+            }
+
+        })
+    })
+
+    // fetch api
+    function getWords() {
+        return fetch(API_URL)
+            .then(res => res.json())
     }
-    console.log(text);
-}
 
-renderWords()
+    // render words
+    async function renderWords() {
+        const words = [];
+        words.push(await getWords());
+        text = "";
+        for (let i = 0; i < words.length; i++)
+        {
+            text += words[i];
+        }
+        str = text.replaceAll(',', ' ');
+        str.split('').forEach(character => {
+            const charSpan = document.createElement('span');
+            charSpan.innerText = character;
+            wordDisplayElement.appendChild(charSpan);
+        })
+    }
+    renderWords();
+    document.getElementById("reset").addEventListener("click", renderWords, true);
+});
+
+
+
+
